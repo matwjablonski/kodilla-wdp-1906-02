b8fb96ba1f73bb8e067303d00d9df10556c7920d
fabianzielinski <fabz@o2.pl>
2019-07-26 01:56:57 +0200

    Add RWD menu.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,3 +4,14 @@ let cartCounterContain = cartCounter.textContent;
 if (cartCounterContain < 0 || cartCounterContain > 999) {
   cartCounter.textContent = 'Err';
 }
+
+var button = document.querySelector('#nav-toggle');
+var menu = document.querySelector('#nav-menu');
+
+button.addEventListener('click', function (event) {
+  if (menu.style.display === '') {
+    menu.style.display = 'block';
+  } else {
+    menu.style.display = '';
+  }
+});
